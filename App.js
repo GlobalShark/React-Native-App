10d115fefa0dcd56ceb415020bdad946c3aac184
Hunnain Pasha <hunnainpashahgchgc@gmail.com>
2018-12-30 23:16:58 +0500

    App Updated

--- App.js
+++ App.js
@@ -1,12 +1,16 @@
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import React, { Component } from 'react';
+import { Provider } from 'react-redux';
+import store from './ReduxStore/store';
+import { Platform, StyleSheet, Text, View } from 'react-native';
 import SplashScreen from './Components/SplashScreen/splashScreen';
 
 
 export default class App extends Component {
   render() {
     return (
+      <Provider store={store}>
         <SplashScreen />
+      </Provider>
     );
   }
 }
